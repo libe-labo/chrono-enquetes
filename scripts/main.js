6bfb62c0e31e8a954ccaa908fe6a1cfd836027c4
Maxime Fabas <m.fabas@liberation.fr>
2018-09-04 18:30:11 +0200

    Actors are now interactive and display bios

--- scripts/main.js
+++ scripts/main.js
@@ -19,15 +19,169 @@ $.ajax({
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
  *
+ *   APPLICATION STATE SETTER
+ *
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+const state = {}
+function setState (key, val) {
+  switch (key) {
+    case 'activeBio':
+      activateBio(val)
+      break
+    case 'actorFilter':
+      console.log('set actor filter on val: ', val)
+      activateFilter(val)
+      break
+    default:
+  }
+  state[key] = val
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *
  *   FILL TEMPLATE WITH DATA & SET INTERACTIONS
  *
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 function init (rawData) {
-  const { actors, facts } = parse(rawData)
   moment.locale('fr')
+  const { actors, facts } = parse(rawData)
 
+  populateTemplate(actors, facts)
+  setState('activeBio', null)
+  setState('actorFilter', null)
+  setInteractions()
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *
+ *   APP COMPONENTS
+ *
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* -------------------- Actor role label -------------------- */
+function ActorRoleLabelTemplate (props) {
+  return $(`
+<div class="actors-panel__role-label">
+  ${props.label}
+</div>`)}
+
+/* -------------------- Actor thumb -------------------- */
+function ActorThumbTemplate (props) {
+  const accusedVariant = props.role === 'Accusé•e' ? 'actor-thumb_accused' : ''
+  const smallVariant = props.small ? 'actor-thumb_small' : ''
+  const noLabelVariant = props.noLabel ? 'actor-thumb_no-label' : ''
+  const openBioVariant = parseInt(props.id, 10) === 1 ? 'actor-thumb_bio-open' : ''
+  return $(`
+<div
+  data-id="${props.id}"
+  class="actor-thumb ${accusedVariant} ${smallVariant} ${noLabelVariant} ${openBioVariant}">
+  <div class="actor-thumb__picture"></div>
+  <div class="actor-thumb__hover-name-wrapper">
+    <div class="actor-thumb__hover-name">${props.name}</div>
+  </div>
+  ${(!smallVariant && ! props.noBio)
+    ? `<div class="actor-thumb__bio">
+      <div class="actor-thumb__bio-name">${props.name}</div>
+      <button class="actor-thumb__bio-close"></button>
+      <div class="actor-thumb__bio-content">${props.bio || 'Aucune bio.'}</div>
+      <button class="actor-thumb__filter-facts">Filtrer</button>
+    </div>`
+    : ''
+  }
+</div>`)}
+
+/* -------------------- Bios panel bio -------------------- */
+function BiosPanelBioTemplate (props) {
+  return $(
+`<div class="bios-panel__bio" data-id="${props.id}">
+  <div class="bios-panel__bio-name">${props.name}</div>
+  <div class="bios-panel__bio-content">${props.bio || 'Aucune bio.'}</div>
+  <button class="bios-panel__filter-facts">Filtrer</button>
+</div>`)}
+
+/* -------------------- Facts year label -------------------- */
+function FactsYearLabelTemplate (props) {
+  return $(`
+<h3 class="facts-panel__year">
+  ${props.year}
+</h3>`)}
+
+/* -------------------- Fact -------------------- */
+function FactTemplate (props) {
+  const relatedActorsDom = props.relatedActors
+    .map(rel => ActorThumbTemplate(
+        Object.assign({}, rel, { small: true })
+      )
+    )
+  return $(`
+<div class="fact">
+  <h4 class="fact__date">${props.date}</h4>
+  <div class="fact__title">${props.title || '!! sans-titre !!'}</div>
+  <div class="fact__card">
+    <div class="fact__content">
+      ${props.content}
+    </div>
+    <div class="fact__actors">
+      ${relatedActorsDom
+        .map(rel => rel[0].outerHTML)
+        .reverse()
+        .join('')}
+    </div>
+  </div>
+</div>`)}
+
+/* -------------------- Fact spacer -------------------- */
+function FactSpacerTemplate (props) {
+  return $(`
+<div
+  class="fact__spacer"
+  style="height: ${props.marginBottom}px">
+</div>`)}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *
+ *   OTHER FUNCTIONS
+ *
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+function parse (rawData) {
+  const rawLines = rawData.split(/\n/)
+  const cells = rawLines.map(line => line.replace(/\r$/, '').split(/\t/))
+  const actorsTable = []
+  const factsTable = []
+  cells.forEach((line, i) => {
+    actorsTable.push(line.slice(0, 5))
+    factsTable.push(line.slice(5))
+  })
+  const actors = table2object(actorsTable)
+  const facts = table2object(factsTable)
+
+  return { actors, facts }
+  
+  function table2object (table) {
+    return table.map((line, i) => {
+      if (i < 3) return
+      if (!line.join('')) return
+      const object = {}
+      line.forEach((elt, j) => {
+        const key = table[1][j]
+        object[key] = elt
+      })
+      return object
+    }).filter(elt => elt)
+  }
+}
+
+function populateTemplate (actors, facts) {
   /* Sort actors bu role */
   const categorizedActors = {}
   actors.forEach(actor => {
@@ -51,6 +205,12 @@ function init (rawData) {
     categorizedActors.accus_e.forEach(actor => {
       ActorThumbTemplate(actor)
         .appendTo('.actors-panel__actors-list')
+      ActorThumbTemplate(Object.assign({}, actor, {
+        noBio: true,
+        noLabel: true
+      })).appendTo('.bios-panel__actors-list')
+      BiosPanelBioTemplate(actor)
+        .appendTo('.bios-panel')
     })
   }
 
@@ -64,6 +224,12 @@ function init (rawData) {
     categorizedActors.plaignant_e.forEach(actor => {
       ActorThumbTemplate(actor)
         .appendTo('.actors-panel__actors-list')
+      ActorThumbTemplate(Object.assign({}, actor, {
+        noBio: true,
+        noLabel: true
+      })).appendTo('.bios-panel__actors-list')
+      BiosPanelBioTemplate(actor)
+        .appendTo('.bios-panel')
     })
   }
 
@@ -77,6 +243,12 @@ function init (rawData) {
     categorizedActors.temoin.forEach(actor => {
       ActorThumbTemplate(actor)
         .appendTo('.actors-panel__actors-list')
+      ActorThumbTemplate(Object.assign({}, actor, {
+        noBio: true,
+        noLabel: true
+      })).appendTo('.bios-panel__actors-list')
+      BiosPanelBioTemplate(actor)
+        .appendTo('.bios-panel')
     })
   }
 
@@ -147,333 +319,58 @@ function init (rawData) {
   })
 }
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- *
- *   APP COMPONENTS
- *
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/* -------------------- Actor role label -------------------- */
-function ActorRoleLabelTemplate (props) {
-  return $(`
-<div class="actors-panel__role-label">
-  ${props.label}
-</div>`)}
-
-/* -------------------- Actor thumb -------------------- */
-function ActorThumbTemplate (props) {
-  const accusedVariant = props.role === 'Accusé•e' ? 'actor-thumb_accused' : null
-  const smallVariant = props.small ? 'actor-thumb_small' : null
-  const name = props.name
-  const id = props.id
-  return $(`
-<button
-  data-id="${id}"
-  class="actor-thumb ${accusedVariant} ${smallVariant}">
-  <div class="actor-thumb__picture"></div>
-  <div class="actor-thumb__hover-name-wrapper">
-    <div class="actor-thumb__hover-name">${name}</div>
-  </div>
-</button>`)}
-
-/* -------------------- Actor bio -------------------- */
-function ActorBioTemplate (props) {
-
-}
-
-/* -------------------- Facts year label -------------------- */
-function FactsYearLabelTemplate (props) {
-  return $(`
-<h3 class="facts-panel__year">
-  ${props.year}
-</h3>`)}
-
-/* -------------------- Fact -------------------- */
-function FactTemplate (props) {
-  const relatedActorsDom = props.relatedActors.map(rel => ActorThumbTemplate(
-    Object.assign(
-      {}, rel, {
-        small: true
-      }
-    ))
-  )
-  return $(`
-<div class="fact">
-  <h4 class="fact__date">${props.date}</h4>
-  <div class="fact__title">${props.title || '!! sans-titre !!'}</div>
-  <div class="fact__card">
-    <div class="fact__content">
-      ${props.content}
-    </div>
-    <div class="fact__actors">
-      ${relatedActorsDom
-        .map(rel => rel[0].outerHTML)
-        .reverse()
-        .join('')}
-    </div>
-  </div>
-</div>`)}
-
-/* -------------------- Fact spacer -------------------- */
-function FactSpacerTemplate (props) {
-  return $(`
-<div
-  class="fact__spacer"
-  style="height: ${props.marginBottom}px">
-</div>`)}
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
- *
- *   OTHER FUNCTIONS
- *
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-function parse (rawData) {
-  const rawLines = rawData.split(/\n/)
-  const cells = rawLines.map(line => line.replace(/\r$/, '').split(/\t/))
-  const actorsTable = []
-  const factsTable = []
-  cells.forEach((line, i) => {
-    actorsTable.push(line.slice(0, 5))
-    factsTable.push(line.slice(5))
-  })
-  const actors = table2object(actorsTable)
-  const facts = table2object(factsTable)
-
-  return { actors, facts }
-  
-  function table2object (table) {
-    return table.map((line, i) => {
-      if (i < 3) return
-      if (!line.join('')) return
-      const object = {}
-      line.forEach((elt, j) => {
-        const key = table[1][j]
-        object[key] = elt
-      })
-      return object
-    }).filter(elt => elt)
+function activateBio (id) {
+  $('.actor-thumb_bio-open').removeClass('actor-thumb_bio-open')
+  if (id === null) {
+    $('.page-content__bios_visible').removeClass('page-content__bios_visible')
+    return
+  }
+  if (parseInt(id)) {
+    if (state.activeBio === null) $('.page-content__bios').addClass('page-content__bios_visible')
+    $(`.actor-thumb[data-id="${id}"]`).addClass('actor-thumb_bio-open')
+    $('.bios-panel__bio_visible').removeClass('bios-panel__bio_visible')
+    $(`.bios-panel__bio[data-id="${id}"`).addClass('bios-panel__bio_visible')
+    const itemWidth = $(`.bios-panel__actors-list .actor-thumb[data-id="${id}"]`).width()
+    const itemOffset = $(`.bios-panel__actors-list .actor-thumb[data-id="${id}"]`).offset().left
+    const listWidth = $('.bios-panel__actors-list').width()
+    const listOffset = $('.bios-panel__actors-list').offset().left
+    const listScroll = $('.bios-panel__actors-list').scrollLeft()
+    const targetOffset = listOffset + listWidth / 2 - itemWidth / 2
+    const offsetDiff = targetOffset - itemOffset
+    const targetScroll = listScroll - offsetDiff
+    $('.bios-panel__actors-list').animate({ scrollLeft: targetScroll }, 400)
+    return
   }
 }
 
+function activateFilter (id) {
+  console.log('activate filter')
+}
 
-// facts.push({
-//   id: '22',
-//   date: '1/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '2/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '24',
-//   date: '4/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '7/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '13/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '18/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '24/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '31/1/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '7/2/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '14/2/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '21/2/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '28/2/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/4/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/6/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2200',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/1/2201',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/6/2201',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/12/2201',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/7/2202',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/3/2203',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/12/2203',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/10/2204',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2205',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2206',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2207',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2209',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2212',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2216',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2221',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2227',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2235',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2244',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2254',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2264',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2284',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2314',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2354',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2404',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2464',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2534',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2614',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2704',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// }, {
-//   id: '23',
-//   date: '1/9/2804',
-//   title: 'In a far away past',
-//   html_text: 'Texte'
-// })
+function setInteractions () {
+  $('.actor-thumb').on('click', function (e) {
+    e.preventDefault()
+    if ($(e.target).hasClass('actor-thumb__picture')) {
+      const actorId = $(this).data('id')
+      setState('activeBio', actorId)
+    }
+  })
+  $('.bios-panel__close').on('click', function (e) {
+    e.preventDefault()
+    setState('activeBio', null)
+  })
+  $('.actor-thumb__bio-close').on('click', function (e) {
+    e.preventDefault()
+    setState('activeBio', null)
+  })
+  $('.actor-thumb__filter-facts').on('click', function (e) {
+    e.preventDefault()
+    const actorId = $(this).parents('.actor-thumb').data('id')
+    setState('actorFilter', actorId)
+  })
+  $('.bios-panel__filter-facts').on('click', function (e) {
+    e.preventDefault()
+    const actorId = $(this).parents('.bios-panel__bio').data('id')
+    setState('actorFilter', actorId)
+  })
+}
