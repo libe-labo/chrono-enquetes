3181b1c130c9757a8a6ebb8ef13d2ab3991c36f4
Maxime Fabas <m.fabas@liberation.fr>
2018-09-05 18:16:49 +0200

    Lots of stuff

--- scripts/main.js
+++ scripts/main.js
@@ -54,7 +54,6 @@ function init (rawData) {
   populateTemplate(actors, facts)
   setState('activeBio', null)
   setState('actorFilter', null)
-  setInteractions()
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -181,7 +180,167 @@ function parse (rawData) {
   }
 }
 
-function populateTemplate (actors, facts) {
+function emptyActorsPanel () {
+  $('.actors-panel__role-label').remove()
+  $('.actors-panel .actor-thumb').remove()
+}
+
+function emptyFactsPanel () {
+  $('.facts-panel__year').remove()
+  $('.facts-panel .fact').remove()
+}
+
+function emptyBiosPanel () {
+  $('.bios-panel .actor-thumb').remove()
+  $('.bios-panel__bio').remove()
+}
+
+function emptyTemplate () {
+  emptyActorsPanel()
+  emptyFactsPanel()
+  emptyBiosPanel()
+}
+
+function populateFactsPanel (actors, facts) {
+  /* Empty facts panel */
+  emptyFactsPanel()
+
+  /* Sort the facts */
+  const sortedFacts = facts.sort((a, b) => {
+    const timestamp = date => moment(date, 'DD/MM/YYYY').format('x')
+    return timestamp(a.date) - timestamp(b.date)
+  })
+
+  /* Add the facts */
+  const actorsArray = (actors => {
+    const ret = []
+    actors.forEach(actor => { ret[actor.id] = actor })
+    return ret
+  })(actors)
+  sortedFacts.forEach((fact, i) => {
+    const date = moment(fact.date, 'DD/MM/YYYY')
+    const pDate = i > 0
+      ? moment(sortedFacts[i - 1].date, 'DD/MM/YYYY')
+      : moment('01/01/0', 'DD/MM/YYYY')
+    const nDate = i < sortedFacts.length - 1
+      ? moment(sortedFacts[i + 1].date, 'DD/MM/YYYY')
+      : date
+    const title = fact.title
+    const content = fact.html_text
+    const diff = nDate.format('x') - date.format('x')
+    const daysDiff = diff / 86400000
+    const calcMargin = days => {
+      const u = 10
+      const [a, b, c, d, e] = [7, 30, 365, 3650, 36500]
+      const [f, g, h, i] = [(b - a), (c - b), (d - c), (e - d)]
+      if (days < 0) return 0
+      if (days < a) return u * days
+      if (days < b) return (u * a) + (days - a) * u/a
+      if (days < c) return (u * a) + (f * u/a) + (days - b) * u/b
+      if (days < d) return (u * a) + (f * u/a) + (g * u/b) + (days - c) * u/c
+      if (days < e) return (u * a) + (f * u/a) + (g * u/b) + (h * u/c) + (days - d) * u/d
+      if (days >= e) return (u * a) + (f * u/a) + (g * u/b) + (h * u/c) + (i * u/d)
+    }
+    const marginBottom = calcMargin(daysDiff)
+    const relatedActors = fact.related_actors_id
+      .split(';')
+      .map(id => parseInt(id, 10) || undefined)
+      .filter(e => e)
+      .map(id => actorsArray[id])
+    if (date.year() - pDate.year()) {
+      $('.facts-panel__end-margin')
+        .before(
+          FactsYearLabelTemplate({
+            year: date.year() 
+          })
+        )
+    }
+    $('.facts-panel__end-margin').before(
+      FactTemplate({
+        title,
+        content,
+        relatedActors,
+        date: date.format('Do MMMM')
+      })
+    )
+    $('.facts-panel__end-margin').before(
+      FactSpacerTemplate({
+        marginBottom
+      })
+    )
+  })
+
+  /* Set event listeners */
+  setInteractions()
+}
+
+function populateBiosPanel (actors, facts) {
+  /* Empty bios panel */
+  emptyBiosPanel()
+
+  /* Sort actors bu role */
+  const categorizedActors = {}
+  actors.forEach(actor => {
+    const role = actor.role
+      .toLowerCase()
+      .replace(/[^a-z0-9-]/igm, '-')
+      .replace(/-{2,}/igm, '-')
+      .replace(/-$/, '')
+      .replace(/-/igm, '_')
+    if (!categorizedActors[role]) categorizedActors[role] = []
+    categorizedActors[role].push(actor)
+  })
+
+  /* Add the accused */
+  if (
+    categorizedActors.accus_e &&
+    categorizedActors.accus_e.length) {
+    categorizedActors.accus_e.forEach(actor => {
+      BiosPanelBioTemplate(actor)
+        .appendTo('.bios-panel')
+      ActorThumbTemplate(Object.assign({}, actor, {
+        noBio: true,
+        noLabel: true
+      })).appendTo('.bios-panel__actors-list')
+    })
+  }
+
+  /* Add the complainants */
+  if (
+    categorizedActors.plaignant_e &&
+    categorizedActors.plaignant_e.length) {
+    categorizedActors.plaignant_e.forEach(actor => {
+      BiosPanelBioTemplate(actor)
+        .appendTo('.bios-panel')
+      ActorThumbTemplate(Object.assign({}, actor, {
+        noBio: true,
+        noLabel: true
+      })).appendTo('.bios-panel__actors-list')
+    })
+  }
+
+  /* Add the witnesses */
+  if (
+    categorizedActors.temoin &&
+    categorizedActors.temoin.length) {
+    categorizedActors.temoin.forEach(actor => {
+      BiosPanelBioTemplate(actor)
+        .appendTo('.bios-panel')
+      ActorThumbTemplate(Object.assign({}, actor, {
+        noBio: true,
+        noLabel: true
+      })).appendTo('.bios-panel__actors-list')
+    })
+  }
+
+  /* Set event listeners */
+  setInteractions()
+}
+
+function populateActorsPanel (actors, facts) {
+  /* Empty actors panel */
+  emptyActorsPanel()
+
   /* Sort actors bu role */
   const categorizedActors = {}
   actors.forEach(actor => {
@@ -205,12 +364,6 @@ function populateTemplate (actors, facts) {
     categorizedActors.accus_e.forEach(actor => {
       ActorThumbTemplate(actor)
         .appendTo('.actors-panel__actors-list')
-      ActorThumbTemplate(Object.assign({}, actor, {
-        noBio: true,
-        noLabel: true
-      })).appendTo('.bios-panel__actors-list')
-      BiosPanelBioTemplate(actor)
-        .appendTo('.bios-panel')
     })
   }
 
@@ -224,12 +377,6 @@ function populateTemplate (actors, facts) {
     categorizedActors.plaignant_e.forEach(actor => {
       ActorThumbTemplate(actor)
         .appendTo('.actors-panel__actors-list')
-      ActorThumbTemplate(Object.assign({}, actor, {
-        noBio: true,
-        noLabel: true
-      })).appendTo('.bios-panel__actors-list')
-      BiosPanelBioTemplate(actor)
-        .appendTo('.bios-panel')
     })
   }
 
@@ -243,104 +390,106 @@ function populateTemplate (actors, facts) {
     categorizedActors.temoin.forEach(actor => {
       ActorThumbTemplate(actor)
         .appendTo('.actors-panel__actors-list')
-      ActorThumbTemplate(Object.assign({}, actor, {
-        noBio: true,
-        noLabel: true
-      })).appendTo('.bios-panel__actors-list')
-      BiosPanelBioTemplate(actor)
-        .appendTo('.bios-panel')
     })
   }
 
-  /* Sort the facts */
-  const sortedFacts = facts.sort((a, b) => {
-    const timestamp = date => moment(date, 'DD/MM/YYYY').format('x')
-    return timestamp(a.date) - timestamp(b.date)
-  })
+  /* Set event listeners */
+  setInteractions()
+}
 
-  /* Add the facts */
-  const calcMargin = days => {
-    const u = 10
-    const [a, b, c, d, e] = [7, 30, 365, 3650, 36500]
-    const [f, g, h, i] = [(b - a), (c - b), (d - c), (e - d)]
-    if (days < 0) return 0
-    if (days < a) return u * days
-    if (days < b) return (u * a) + (days - a) * u/a
-    if (days < c) return (u * a) + (f * u/a) + (days - b) * u/b
-    if (days < d) return (u * a) + (f * u/a) + (g * u/b) + (days - c) * u/c
-    if (days < e) return (u * a) + (f * u/a) + (g * u/b) + (h * u/c) + (days - d) * u/d
-    if (days >= e) return (u * a) + (f * u/a) + (g * u/b) + (h * u/c) + (i * u/d)
-  }
-  const actorsArray = (actors => {
-    const ret = []
-    actors.forEach(actor => {
-      ret[actor.id] = actor
-    })
-    return ret
-  })(actors)
-  sortedFacts.forEach((fact, i) => {
-    const date = moment(fact.date, 'DD/MM/YYYY')
-    const pDate = i > 0
-      ? moment(sortedFacts[i - 1].date, 'DD/MM/YYYY')
-      : moment('01/01/0', 'DD/MM/YYYY')
-    const nDate = i < sortedFacts.length - 1
-      ? moment(sortedFacts[i + 1].date, 'DD/MM/YYYY')
-      : date
-    const title = fact.title
-    const content = fact.html_text
-    const diff = nDate.format('x') - date.format('x')
-    const daysDiff = diff / 86400000
-    const marginBottom = calcMargin(daysDiff)
-    const relatedActors = fact.related_actors_id
-      .split(';')
-      .map(id => parseInt(id, 10) || undefined)
-      .filter(e => e)
-      .map(id => actorsArray[id])
-    if (date.year() - pDate.year()) {
-      $('.facts-panel__end-margin').before(
-        FactsYearLabelTemplate({
-          year: date.year()
-        })
-      )
-    }
-    $('.facts-panel__end-margin').before(
-      FactTemplate({
-        title,
-        content,
-        relatedActors,
-        date: date.format('Do MMMM')
-      })
-    )
-    $('.facts-panel__end-margin').before(
-      FactSpacerTemplate({
-        marginBottom
-      })
-    )
-  })
+function populateTemplate (actors, facts) {
+  /* Empty template */
+  emptyTemplate()
+
+  /* Populate app panels */
+  populateActorsPanel(actors, facts)
+  populateBiosPanel(actors, facts)
+  populateFactsPanel(actors, facts)
+
+  /* Set event listeners */
+  setInteractions()
 }
 
 function activateBio (id) {
+  // If already active, do nothing, STOP HERE
+  if (id === state.activeBio) return
+  // Close opened bio in actors panel (desktop)
   $('.actor-thumb_bio-open').removeClass('actor-thumb_bio-open')
+  // If 'null' to activate, hide bios panel (mobile), STOP HERE
   if (id === null) {
-    $('.page-content__bios_visible').removeClass('page-content__bios_visible')
-    return
+    $('.page-content__bios_visible')
+      .animate({ opacity: 0 }, 400, e => {
+        $('.page-content__bios_visible')
+          .removeClass('page-content__bios_visible')
+          .attr('style', '')
+      })
+    return 
   }
-  if (parseInt(id)) {
-    if (state.activeBio === null) $('.page-content__bios').addClass('page-content__bios_visible')
-    $(`.actor-thumb[data-id="${id}"]`).addClass('actor-thumb_bio-open')
-    $('.bios-panel__bio_visible').removeClass('bios-panel__bio_visible')
-    $(`.bios-panel__bio[data-id="${id}"`).addClass('bios-panel__bio_visible')
-    const itemWidth = $(`.bios-panel__actors-list .actor-thumb[data-id="${id}"]`).width()
-    const itemOffset = $(`.bios-panel__actors-list .actor-thumb[data-id="${id}"]`).offset().left
-    const listWidth = $('.bios-panel__actors-list').width()
-    const listOffset = $('.bios-panel__actors-list').offset().left
-    const listScroll = $('.bios-panel__actors-list').scrollLeft()
-    const targetOffset = listOffset + listWidth / 2 - itemWidth / 2
-    const offsetDiff = targetOffset - itemOffset
-    const targetScroll = listScroll - offsetDiff
-    $('.bios-panel__actors-list').animate({ scrollLeft: targetScroll }, 400)
-    return
+  // Stop all animations and remove style attributes added by jQuery animations
+  $('.bios-panel__bio').stop().attr('style', '')
+  // If no bio is currently active, show the bios panel (mobile) 
+  if (state.activeBio === null) {
+    $('.page-content__bios')
+      .addClass('page-content__bios_visible')
+      .css({ opacity: 0 })
+      .animate({ opacity: 1 }, 400, e => {
+        $('.page-content__bios')
+          .attr('style', '')
+      })
   }
+  // Open the bios on the right actor thumbs
+  $(`.actor-thumb[data-id="${id}"]`)
+    .addClass('actor-thumb_bio-open')
+  // Re-arrange bios order in bios panel (mobile) (so that they appear above the prev one)
+  const newOrder = []
+  $('.bios-panel__bio').each((i, elt) => {
+    if ($(elt).data('id') !== id) newOrder.push(elt)
+  })
+  newOrder.unshift($(`.bios-panel__bio[data-id="${id}"`))
+  $('.bios-panel__bio').detach()
+  newOrder.forEach(elt => $('.bios-panel').append(elt))
+  // Animate previous bio and next bio in bios panel (mobile)
+  const prevPanel = $('.bios-panel__bio_visible')
+  const newPanel = $(`.bios-panel__bio[data-id="${id}"`)
+  if (prevPanel.length) {
+    prevPanel.animate({ opacity: 0 }, 400, e => {
+      prevPanel.animate({ height: 0 }, 400, e => {
+        prevPanel
+          .removeClass('bios-panel__bio_visible')
+          .attr('style', '')
+      })
+      newPanel
+        .addClass('bios-panel__bio_visible')
+        .hide()
+        .slideDown(400, e => newPanel.attr('style', ''))
+    })
+  } else {
+    newPanel
+      .hide()
+      .slideDown(400, e => newPanel.addClass('bios-panel__bio_visible').attr('style', ''))
+  }
+  // Center actor thumbs list in bios panel (mobile)
+  const bioPan = {}
+  bioPan.itemWidth = $(`.bios-panel__actors-list .actor-thumb[data-id="${id}"]`).width(),
+  bioPan.itemOffset = $(`.bios-panel__actors-list .actor-thumb[data-id="${id}"]`).offset().left,
+  bioPan.listWidth = $('.bios-panel__actors-list').width(),
+  bioPan.listOffset = $('.bios-panel__actors-list').offset().left,
+  bioPan.listScroll = $('.bios-panel__actors-list').scrollLeft(),
+  bioPan.targetOffset = bioPan.listOffset + bioPan.listWidth / 2 - bioPan.itemWidth / 2,
+  bioPan.offsetDiff = bioPan.targetOffset - bioPan.itemOffset,
+  bioPan.targetScroll = bioPan.listScroll - bioPan.offsetDiff
+  $('.bios-panel__actors-list').animate({ scrollLeft: bioPan.targetScroll }, 400)
+  // Center actor thumbs list in actors-list (mobile)
+  const actPan = {}
+  actPan.itemWidth = $(`.actors-panel__actors-list .actor-thumb[data-id="${id}"]`).width(),
+  actPan.itemOffset = $(`.actors-panel__actors-list .actor-thumb[data-id="${id}"]`).offset().left,
+  actPan.listWidth = $('.actors-panel__actors-list').width(),
+  actPan.listOffset = $('.actors-panel__actors-list').offset().left,
+  actPan.listScroll = $('.actors-panel__actors-list').scrollLeft(),
+  actPan.targetOffset = actPan.listOffset + actPan.listWidth / 2 - actPan.itemWidth / 2,
+  actPan.offsetDiff = actPan.targetOffset - actPan.itemOffset,
+  actPan.targetScroll = actPan.listScroll - actPan.offsetDiff
+  $('.actors-panel__actors-list').animate({ scrollLeft: actPan.targetScroll }, 400)
 }
 
 function activateFilter (id) {
